ba87d0d35e1b070a040c3141ad615d81c5b5a57f
michaelzito <mzito@udel.edu>
2015-11-20 17:09:44 -0500

    Added feedback api and some missing semicolons

--- server/config/routes.js
+++ server/config/routes.js
@@ -53,19 +53,36 @@ module.exports = function(app, passport) {
   });
 
   app.post('/deleteAssignment', function(req, res) {
-    assignment.deleteAssignment(req, res)
+    assignment.deleteAssignment(req, res);
   });
 
   app.post('/updateAssignment', function(req, res) {
-    assignment.updateAssignment(req, res)
+    assignment.updateAssignment(req, res);
   });
 
   app.post('/getAssignment', function(req, res, next) {
-    assignment.getAssignment(req, res, next)
+    assignment.getAssignment(req, res, next);
   });
 
   app.post('/getAllAssignments', function(req, res, next) {
     assignment.getAllAssignments(req, res, next);
+  });
+
+
+  app.post('/createFeedback', function(req, res, next) {
+    feedback.createFeedback(req, res, next);
+  });
+
+  app.post('/deleteFeedback', function(req, res) {
+    feedback.deleteFeedback(req, res);
+  });
+
+  app.post('/updateFeedback', function(req, res) {
+    feedback.updateFeedback(req, res);
+  });
+
+  app.post('/getFeedback', function(req, res, next) {
+    feedback.getFeedback(req, res, next);
   });
 
   // google auth
--- server/controllers/feedbackapi.js
+++ server/controllers/feedbackapi.js
@@ -6,3 +6,73 @@ var Feedback = require('../models/feedback');
 var UserProblemPair = require('../models/userproblempair');
 
 //POST GET API for feedback
+
+
+//create feedback
+exports.createFeedback = function(req, res, next) {
+	console.log(req.body);
+	var lineFeedback = req.body.lineFeedback;
+	var ParsonsProblemReference = req.body.ParsonsProblemReference;
+	for (var i = 0; i < lineFeedback.length; i++) {
+		if (lineFeedback[i] != "") {
+			newFeedback.feedback.push(lineFeedback[i]);
+		}
+	}
+	newFeedback.save();
+	console.log(newFeedback);
+	res.json(req.body);
+};
+
+exports.getFeedback = function(req, res, next) {
+	console.log(req.body);
+	var id = req.body.id;
+	feedback.findById(id, function(err, feedback) {
+		if(!err) {
+			res.json(feedback);
+		}
+		else {
+			console.log("Couldn't find the feedback");
+		}
+	});
+};
+
+//update existing feedback
+exports.updateFeedback = function(req, res) {
+	var id = req.body.id;
+	var lineFeedback = req.body.lineFeedback;
+	var ParsonsProblemReference = req.body.ParsonsProblemReference;
+
+	feedback.findById(id, function(err, feedback) {
+		if(err) {
+			res.redirect('/createProblem');
+			console.log("Error on update");
+		}
+		else {
+			if (feedback.lineFeedback != lineFeedback) {
+				feedback.lineFeedback = lineFeedback;
+			}
+			if (feedback.ParsonsProblemReference != ParsonsProblemReference) {
+				feedback.ParsonsProblemReference = ParsonsProblemReference;
+			}
+			feedback.save();
+			res.end();
+		}
+	});
+};
+
+// delete existing feedback
+exports.deleteFeedback = function(req, res) {
+	var id = req.body.id;
+	Feeback.findByIdAndRemove(id, function(err, feedback) {
+		if(err) {
+			console.log('Error on delete');
+			res.redirect('/createProblem');
+		}
+		else {
+			if (feedback != null) {
+				res.status(200).send('Removed Successfully');
+			}
+		}
+		res.end();
+	});
+};
